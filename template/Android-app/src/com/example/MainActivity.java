1c3f6e99e54be959dc2fb685da1bcdf62b4222d1
gdsglgf <wzguofulgz@126.com>
2016-10-15 20:08:15 +0800

    set menu icon visible in Android 4.0

--- template/Android-app/src/com/example/MainActivity.java
+++ template/Android-app/src/com/example/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example;
 
+import java.lang.reflect.Method;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 public class MainActivity extends Activity {
     @Override
@@ -11,11 +14,27 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod(
+                            "setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);   // set menu icon visible in Android 4.0
+                } catch (Exception e) {
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
     }
 
     @Override
